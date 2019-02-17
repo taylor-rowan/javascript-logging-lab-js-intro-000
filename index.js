ec52842e87022d85e49d98eb116283eda5a55f3d
Rowan Taylor <rowan.taylor@colorado.edu>
2019-02-17 22:30:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 // calling console error
 
 console.error("HELP!")
+
+// calling console warn
+console.warn("Be careful.")
+
+// calling console log (notification)
+console.log("Logging successful.")
+
